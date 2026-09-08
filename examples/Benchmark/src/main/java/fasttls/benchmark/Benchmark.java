e049a8b7211450ae6f3e51abe7fb054c07ae23f5
Andre Stubbe <andrestubbe@proton.me>
2026-09-08 11:23:48 +0200

    Align examples with repository structure

--- examples/Benchmark/src/main/java/fasttls/benchmark/Benchmark.java
+++ examples/Benchmark/src/main/java/fasttls/benchmark/Benchmark.java
@@ -3,7 +3,7 @@ package fasttls.benchmark;
 import fasttls.FastTLS;
 
 public final class Benchmark {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         long start = System.nanoTime();
         FastTLS.connect("example.com", 443).join().close();
         System.out.printf("TLS handshake completed in %.3f ms%n", (System.nanoTime() - start) / 1_000_000.0);
